746dfc07192070639bb1ad01a8b4fc215a34007d
panhayuth <panhayuth11@gmail.com>
2024-10-24 14:01:39 +0700

    fix: list table accordion

--- listTable.php
+++ listTable.php
@@ -38,54 +38,62 @@
 
 <div class="card mb-4 shadow-sm" id="tableCard">
     <div class="card-header">
-        <h4 class="my-0 font-weight-normal">Lists Overview</h4>
+        <div class="row d-flex justify-content-md-between row-cols-1 row-cols-sm-2">
+            <div class="col">
+                <h4 class="my-sm-0 font-weight-normal">Lists Overview</h4>
+            </div>
+            <div class="col d-flex justify-content-sm-end">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listCreateModal">
+                    Create A List
+                </button>
+            </div>
+        </div>
     </div>
     <div class="card-body">
-        <form method="get">
-            <div class="row justify-content-md-between row-cols-1 row-cols-sm-2">
-                <div class="mb-3 col-xl-6">
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listCreateModal">
-                        Create A List
-                    </button>
-                </div>
-                <div class="mb-3 col-sm-8 col-xl-4">
-                    <input type="text" name="search_query" class="form-control w-100" placeholder="Search tasks..." value="<?php echo isset($_GET['search_query']) ? htmlspecialchars($_GET['search_query']) : ''; ?>">
-                </div>
-                <div class="mb-3 col-sm-4 col-xl-2">
-                    <button type="submit" class="btn btn-outline-primary w-100">Apply</button>
-                </div>
-            </div>
-        </form>
-
         <?php require 'createList.php'; ?>
 
-        <div class="table-responsive">
-            <table class="table table-bordered" id="taskTable">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($lists as $list) { ?>
-                        <tr>
-                            <td><?= htmlspecialchars($list['title']) ?></td>
-                            <td>
-                                <div class="dropdown">
-                                    <a class='link' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
-                                        <i class='fas fa-ellipsis-v'></i>
-                                    </a>
-                                    <ul class='dropdown-menu'>
-                                        <li><a class='dropdown-item' data-bs-toggle='modal' data-bs-target='#editListModal<?= $list['id'] ?>'>Edit List</a></li>
-                                        <li><a class='dropdown-item' href='deleteList.php?id=<?= $list['id'] ?>' onclick='return confirm("Are you sure you want to delete this list?");'>Delete List</a></li>
-                                    </ul>
-                                </div>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+        <div class="accordion" id="accordionList">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                        My Lists
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionList">
+                    <div class="accordion-body">
+                        <li class="list-group-item">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="taskTable">
+                                    <thead>
+                                        <tr>
+                                            <th>Title</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($lists as $list) { ?>
+                                            <tr>
+                                                <td><?= htmlspecialchars($list['title']) ?></td>
+                                                <td>
+                                                    <div class="dropdown">
+                                                        <a class='link' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+                                                            <i class='fas fa-ellipsis-v'></i>
+                                                        </a>
+                                                        <ul class='dropdown-menu'>
+                                                            <li><a class='dropdown-item' data-bs-toggle='modal' data-bs-target='#editListModal<?= $list['id'] ?>'>Edit List</a></li>
+                                                            <li><a class='dropdown-item' href='deleteList.php?id=<?= $list['id'] ?>' onclick='return confirm("Are you sure you want to delete this list?");'>Delete List</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </li>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
